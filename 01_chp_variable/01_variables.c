ead66610f47a245bc2848555e1cf0e7a01e266a7
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-12-03 12:31:38 +0000

    01 variables Done

---
+++ 01_chp_variable/01_variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    int a; // variable decleration
+    a = 6; // variable initilization
+
+    printf("The output of program is %d", a); // variable execution
+    return 0;
+}

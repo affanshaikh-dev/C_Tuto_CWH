0a3d2dc2aef04185b6e0293b51b810fb1f61d37d
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-12-04 15:22:55 +0000

    practice set 2 done

--- 02_chp/04_practice_set/04_ps.c
+++ 02_chp/04_practice_set/04_ps.c
@@ -1,6 +1,19 @@
-// 3.0 + 1 will be:
-// a. Integer.
-// b. Floating point number.
-// c. Character.
+#include<stdio.h>
 
-// ans: (b)Floating point number.
+int main() {
+    int x = 2, y = 3, z = 3, k = 1;
+    float e = 3*x/y - z+k;
+
+    // detail solution
+    /* 
+    = 3*2/3 - 3+1
+    = 6/3 - 3+1
+    = 2 - 3+1
+    = -1 + 1
+    = 0.000
+    */
+
+    printf("%f", e);
+
+    return 0;
+}

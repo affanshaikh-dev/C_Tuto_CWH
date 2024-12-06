a3f2050fb678b64d7e4f72a2235d63d332be269b
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-12-06 06:06:04 +0000

    03 Chp Practice Set Done

---
+++ 03_chp/07_practice_set/06_ps.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3, num4, greatest;
+
+    printf("Enter four numbers: ");
+    scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
+
+    greatest = 
+               : (num2 > num3  (num1 > num2) ? 
+               (num1 > num3 ? 
+                   (num1 > num4 ? num1 : num4) 
+                   : (num3 > num4 ? num3 : num4))? 
+                   (num2 > num4 ? num2 : num4) 
+                   : (num3 > num4 ? num3 : num4));
+
+    printf("The greatest of the four numbers is: %d\n", greatest);
+
+    return 0;
+}

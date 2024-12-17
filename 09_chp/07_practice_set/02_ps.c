14d409f1089474e875a65f07833f2dac907f6f87
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-12-17 12:21:34 +0000

    08 practice set on going

---
+++ 09_chp/07_practice_set/02_ps.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct vector {
+    int i;
+    int j;
+} vec;
+
+int main() {
+    vec v = {1, 2};
+    printf("The value of vector is %di + %dj", v.i, v.j);
+    
+    return 0;
+}

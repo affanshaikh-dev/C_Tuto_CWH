fc1790d0b7ee1baa46f40860a252ae3ff1ac672d
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-12-20 13:35:14 +0000

    Project 02 Done

--- 10_project_s_w_g/index.c
+++ 10_project_s_w_g/index.c
@@ -2,11 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
+    char con;
     srand(time(0));
-    int player, computer = rand() % 3;
+    int player, computer;
 
-    printf("Choose ");
-    
+    do
+    {
+        computer = rand() % 3;
+
+        printf("Welcome to Snake, Water, Gun Game\n0. Snake\n1. Water\n2. Gun\n");
+        scanf("%d", &player);
+
+        if (computer == player)
+        {
+            printf("Tie! Both are Same.\n");
+        }
+        else if ((player == 0 && computer == 1) || (player == 1 && computer == 2) || (player == 2 && computer == 0))
+        {
+            printf("You Win!\n");
+        }
+        else
+        {
+            printf("Computer Wins!\n");
+        }
+
+        printf("You want play again (y/n) ");
+        while ((getchar()) != '\n');
+        scanf("%c", &con);
+    } while (con == 'Y' || con == 'y');
+
     return 0;
 }
